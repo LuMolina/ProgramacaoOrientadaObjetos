02db636540a4e484ecb42ac4b6012c4c572babab
LuMolina <90227193+LuMolina@users.noreply.github.com>
2021-12-01 11:51:47 -0300

    novo commit com digitação de entrada

--- Program.cs
+++ Program.cs
@@ -14,8 +14,16 @@ objetoPessoa.Sobrenome = "Molina";
 Console.WriteLine($"O nome da pessoa é: {objetoPessoa.Nome} {objetoPessoa.Sobrenome}");
 //get é responsável pela leitura dessas propriedades. 
 
+Console.WriteLine("Digite a sua data de nascimento: (dd/MM/aaaa) ");
+string? textoDataNascimento = Console.ReadLine();
+int ano = Convert.ToInt32(textoDataNascimento?.Substring(6, 4));
+int mes = Convert.ToInt32(textoDataNascimento?.Substring(3, 2));
+int dia = Convert.ToInt32(textoDataNascimento?.Substring(0, 2));
+//subrtring pega o índice inicial, quantidade de carateres que ele quer pegar a partir dali.
 
-Pessoa novoObjetoPessoa = new("Luciane", "Molina", new DateTime(1982, 12, 08));
+Pessoa novoObjetoPessoa = new("Luciane", "Molina", new DateTime(ano, mes, dia));
+
+
 Console.WriteLine($"O nome da pessoa do novo objeto é: {novoObjetoPessoa.Nome} {novoObjetoPessoa.Sobrenome}. {novoObjetoPessoa.CalculaIdade()} ");
 
 
